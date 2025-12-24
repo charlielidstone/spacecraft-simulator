d7c79f3c20b64a25de14d2b9f7b91c39cc0f03b9
Charlie Lidstone <88647310+charlielidstone@users.noreply.github.com>
2025-12-23 22:10:23 -0800

    add dependencies

--- SpacecraftSimulator/main.cpp
+++ SpacecraftSimulator/main.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 #include "simulation/Simulation.hpp"
 
-
 int main(void) {
+
+	constexpr auto WINDOW_WIDTH = 800;
+	constexpr auto WINDOW_HEIGHT = 600;
+
+	sf::RenderWindow window(sf::VideoMode({ WINDOW_WIDTH, WINDOW_HEIGHT }), "Spacecraft Simulator");
 
 	double timestep = (1 / 60.0);
 	Simulation sim(timestep);
 
-	for (int i = 0; i < 300; i++) {
-		sim.step();
-		const auto& state = sim.getState();
+	constexpr auto ROCKET_RADIUS = 5.0F;
+	sf::CircleShape rocket(ROCKET_RADIUS);
+	rocket.setFillColor(sf::Color::White);
 
-		std::cout
-			<< "t = " << state.time
-			<< ", y = " << state.body.position.y
-			<< ", Vy = " << state.body.velocity.y
-			<< std::endl;
+	while (window.isOpen()) {
+		while (const std::optional event = window.pollEvent()) {
+			if (event->is<sf::Event::Closed>()) {
+				window.close();
+			}
+		}
+
+		sim.step();
+
+		const WorldState& state = sim.getState();
+
+		rocket.setPosition({
+				static_cast<float>(state.body.position.x),
+				static_cast<float>(600.0 - state.body.position.y)
+			});
+
+		window.clear();
+		window.draw(rocket);
+		window.display();
 	}
 
 	return 0;
