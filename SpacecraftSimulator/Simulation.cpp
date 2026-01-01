2d09e02a7e39b857a00a8073fcc3668a49be0909
Charlie Lidstone <88647310+charlielidstone@users.noreply.github.com>
2025-12-31 16:01:29 -0800

    we now add objects by adding them to simulation

--- SpacecraftSimulator/Simulation.cpp
+++ SpacecraftSimulator/Simulation.cpp
@@ -9,7 +9,7 @@ Simulation::Simulation(double timestep) : dt(timestep) {
 	state.time = 0.0;
 
 	RigidBody rocket = {
-		.id = 0,
+		.id = nextID++,
 		.name = "rocket",
 		.mass = 100.0,
 		.width = 10.0,
@@ -19,7 +19,33 @@ Simulation::Simulation(double timestep) : dt(timestep) {
 		.throttle = 0.0
 	};
 
-	state.bodies.emplace(nextID++, rocket);
+	state.bodies.emplace(rocket.id, rocket);
+
+	RigidBody otherRocket = {
+		.id = nextID++,
+		.name = "otherRocket",
+		.mass = 100.0,
+		.width = 30.0,
+		.height = 90.0,
+		.position = { -10.0, -30.0 },
+		.velocity = { 0.0, 40.0 },
+		.throttle = 0.0
+	};
+
+	state.bodies.emplace(otherRocket.id, otherRocket);
+
+	RigidBody rocket3 = {
+		.id = nextID++,
+		.name = "rocket3",
+		.mass = 100.0,
+		.width = 40.0,
+		.height = 20.0,
+		.position = { -40.0, 30.0 },
+		.velocity = { 30.0, 0.0 },
+		.throttle = 0.0
+	};
+
+	state.bodies.emplace(rocket3.id, rocket3);
 
 	forces.push_back(std::make_unique<GravityForce>(Vector2{ 0.0, -9.81 }));
 	forces.push_back(std::make_unique<ThrustForce>(10000, 0));
@@ -35,44 +61,31 @@ void Simulation::simulateGpuDelay() {
 }
 
 void Simulation::step(InputState& input) {
-	Vector2 netForce{ 0.0, 0.0 };
-	double netTorque = 0.0;
-
-	if (input.shiftPressed || input.WPressed) {
-		state.bodies[0].throttle = 1.0;
-	} else if (input.SPressed) {
-		state.bodies[0].throttle = -1.0;
-	} else {
-		state.bodies[0].throttle = 0.0;
-	}
-
-	if (input.APressed) {
-		state.bodies[0].angle -= (3.1415926535 / 180);
-	} else if (input.DPressed) {
-		state.bodies[0].angle += (3.1415926535 / 180);
-	}
-
-	for (const auto& force : forces) {
-		netForce += force->computeForce(state.bodies[0]);
-		netTorque += force->computeTorque(state.bodies[0]);
-	}
-
-	Vector2 acceleration = netForce / state.bodies[0].mass;
-	state.bodies[0].velocity += acceleration * dt;
-	state.bodies[0].position += state.bodies[0].velocity * dt;
-	
-	std::cout << "Net Force: (" << netForce.x << ", " << netForce.y << ")\n";
-	std::cout << "Mass: " << state.bodies[0].mass << "\n";
-	std::cout << "Position: (" << state.bodies[0].position.x << ", " << state.bodies[0].position.y << ")\n";
-	std::cout << "Velocity: (" << state.bodies[0].velocity.x << ", " << state.bodies[0].velocity.y << ")\n";
-	std::cout << "Acceleration: (" << acceleration.x << ", " << acceleration.y << ")\n";
-	std::cout << "Rotation: " << (state.bodies[0].angle * 180 / 3.1415926535) << "\n";
-	std::cout << "dt: " << dt << "\n";
-
-	state.time += dt;
-
 	std::cout << "Frame count: " << count++ << "\n";
 	std::cout << "-------------------\n";
+	for (auto& [id, body] : state.bodies) {
+		Vector2 netForce{ 0.0, 0.0 };
+		double netTorque = 0.0;
+
+		Vector2 acceleration = netForce / body.mass;
+		body.velocity += acceleration * dt;
+		body.position += body.velocity * dt;
+	
+
+		std::cout << "Object: " << body.name << "\n";
+		std::cout << "Net Force: (" << netForce.x << ", " << netForce.y << ")\n";
+		std::cout << "Mass: " << body.mass << "\n";
+		std::cout << "Position: (" << body.position.x << ", " << body.position.y << ")\n";
+		std::cout << "Velocity: (" << body.velocity.x << ", " << body.velocity.y << ")\n";
+		std::cout << "Acceleration: (" << acceleration.x << ", " << acceleration.y << ")\n";
+		std::cout << "Rotation: " << (body.angle * 180 / 3.1415926535) << "\n";
+		std::cout << "dt: " << dt << "\n";
+
+		state.time += dt;
+
+		std::cout << "-------------------\n";
+	}
+
 
 	//simulateGpuDelay();
 }
